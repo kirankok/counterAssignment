6efb39271cb50f4f576a71747d0a0371facdc824
kirankok <kirankokumar@gmail.com>
2020-05-23 14:48:30 +0530

    Added test cases for App component

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 <------Counter Demo App------>
+
+Instructions:
+
+*) Run "npm install" in the root of the folder.
+*) Run "npm start" to run the app at localhost:3000.
+*) Run "npm test" to run the test cases.
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('span').text();
+    expect(text).toEqual('0');
+  });
+
+  it('increments count by 1 when the increment button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.button').at(0);
+    incrementBtn.simulate('click');
+    const text = wrapper.find('span').text();
+    expect(text).toEqual('1');
+  });
+
+  it('decrements count by 1 when the decrement button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.button').at(1);
+    incrementBtn.simulate('click');
+    const text = wrapper.find('span').text();
+    expect(text).toEqual('0');
+  });
+
+  it('count value is not decremented below 0', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.button').at(1);
+    incrementBtn.simulate('click');
+    const text = wrapper.find('span').text();
+    expect(text).toEqual('0');
+  });
 });
+
